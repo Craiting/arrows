f241659ce9667b07be51fd87c7e4e5b4dc0ded5c
Craig Tingey <craig@decisely.com>
2018-01-23 20:35:31 -0700

    Move left or right with gamepad.

---
+++ src/js/gamepad.js
@@ -0,0 +1,18 @@
+console.log('hey');
+
+var gamepads = {};
+
+function gamepadHandler(event, connecting) {
+  var gamepad = event.gamepad;
+  // Note:
+  // gamepad === navigator.getGamepads()[gamepad.index]
+
+  if (connecting) {
+    gamepads[gamepad.index] = gamepad;
+  } else {
+    delete gamepads[gamepad.index];
+  }
+}
+
+window.addEventListener("gamepadconnected", function(e) { gamepadHandler(e, true); }, false);
+window.addEventListener("gamepaddisconnected", function(e) { gamepadHandler(e, false); }, false);
